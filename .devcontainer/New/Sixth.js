2e3f454f2728967e1734174037b168abd7902e3e
Sidhant001 <143825824+Sidhant001@users.noreply.github.com>
2026-01-27 12:07:51 +0000

    Type Conversion

---
+++ .devcontainer/New/Sixth.js
@@ -0,0 +1,13 @@
+//a) Convert both and calculate area of rectangle:
+
+let width = "10";
+let height = "5";
+let Areaofrect = Number(width)*Number(height)
+console.log(Areaofrect)
+
+//b) Print correct sum:
+
+let a = 10;
+let b = "20";
+let Sum = a + Number(b)
+console.log(Sum)
